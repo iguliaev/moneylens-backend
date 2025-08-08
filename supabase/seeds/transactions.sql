6ff467d1db84a972e700bead465e2c0d47e5be8a
Igor Guliaev <igor.s.gulyaev@gmail.com>
2025-08-08 22:27:22 +0100

    Add bank_account columnt to transactions table

--- supabase/seeds/transactions.sql
+++ supabase/seeds/transactions.sql
@@ -1,4 +1,4 @@
-INSERT INTO transactions (id, date, type, category, amount, tags, notes)
+INSERT INTO transactions (id, date, type, category, amount, tags, notes, bank_account)
 SELECT
   uuid_generate_v4(),
   CURRENT_DATE - (random() * 365)::int,
@@ -8,5 +8,6 @@ SELECT
   ARRAY[
     (ARRAY['groceries', 'bonus', 'movie', 'bus', 'doctor', 'stocks', 'birthday', 'clothes'])[floor(random() * 8 + 1)]
   ],
-  'Sample note ' || (generate_series(1,100))
+  'Sample note ' || (generate_series(1,100)),
+  (ARRAY['Chase', 'Bank of America', 'Wells Fargo', 'Ally', 'Capital One', 'Revolut', 'Monzo', 'Wise'])[floor(random() * 8 + 1)]
 FROM generate_series(1,100);
