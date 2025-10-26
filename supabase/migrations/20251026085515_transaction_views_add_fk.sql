b47070fea7026f8e8256b94593719ab9e2ef6789
Igor Guliaev <igor.s.gulyaev@gmail.com>
2025-10-26 08:58:31 +0000

    Add migration file

---
+++ supabase/migrations/20251026085515_transaction_views_add_fk.sql
@@ -0,0 +1,64 @@
+drop view if exists "public"."transactions_earn";
+
+drop view if exists "public"."transactions_save";
+
+drop view if exists "public"."transactions_spend";
+
+create or replace view "public"."transactions_earn" as  SELECT t.id,
+    t.user_id,
+    t.date,
+    t.type,
+    t.category_id,
+    COALESCE(t.category, c.name) AS category,
+    t.bank_account_id,
+    COALESCE(t.bank_account, b.name) AS bank_account,
+    t.amount,
+    t.tags,
+    t.notes,
+    t.created_at,
+    t.updated_at
+   FROM ((transactions t
+     LEFT JOIN bank_accounts b ON ((t.bank_account_id = b.id)))
+     LEFT JOIN categories c ON ((t.category_id = c.id)))
+  WHERE (t.type = 'earn'::transaction_type);
+
+
+create or replace view "public"."transactions_save" as  SELECT t.id,
+    t.user_id,
+    t.date,
+    t.type,
+    t.category_id,
+    COALESCE(t.category, c.name) AS category,
+    t.bank_account_id,
+    COALESCE(t.bank_account, b.name) AS bank_account,
+    t.amount,
+    t.tags,
+    t.notes,
+    t.created_at,
+    t.updated_at
+   FROM ((transactions t
+     LEFT JOIN bank_accounts b ON ((t.bank_account_id = b.id)))
+     LEFT JOIN categories c ON ((t.category_id = c.id)))
+  WHERE (t.type = 'save'::transaction_type);
+
+
+create or replace view "public"."transactions_spend" as  SELECT t.id,
+    t.user_id,
+    t.date,
+    t.type,
+    t.category_id,
+    COALESCE(t.category, c.name) AS category,
+    t.bank_account_id,
+    COALESCE(t.bank_account, b.name) AS bank_account,
+    t.amount,
+    t.tags,
+    t.notes,
+    t.created_at,
+    t.updated_at
+   FROM ((transactions t
+     LEFT JOIN bank_accounts b ON ((t.bank_account_id = b.id)))
+     LEFT JOIN categories c ON ((t.category_id = c.id)))
+  WHERE (t.type = 'spend'::transaction_type);
+
+
+
